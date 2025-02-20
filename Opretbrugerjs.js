d2ebf885d9eda54107865525795f0f3309e37533
Roman Ghazi <rog001@edu.zealand.dk>
2025-02-20 19:57:08 +0100

    Felter er required og nulstillinger af beskeder

--- Opretbrugerjs.js
+++ Opretbrugerjs.js
@@ -13,9 +13,11 @@ Vue.createApp({
     },
     methods: {
         async signUpUser() {
+            
             this.errorMessage = null;
             this.successMessage = null;
 
+            
             if (this.password !== this.confirmPassword) {
                 this.errorMessage = "Passwords do not match";
                 return;
@@ -24,12 +26,23 @@ Vue.createApp({
             try {
                 await axios.post(URI, {
                     username: this.username,
-                    email: this.email,   
-                    password: this.password,
-                    role: 0             
+                    email: this.email,
+                    password: this.password
                 });
+
+               
+                this.errorMessage = null;
                 this.successMessage = "User created successfully!";
+
+                
+                this.username = '';
+                this.email = '';
+                this.password = '';
+                this.confirmPassword = '';
+
             } catch (error) {
+                
+                this.successMessage = null;
                 this.errorMessage = "Failed to create user: " + (error.response?.data || error.message);
             }
         }
